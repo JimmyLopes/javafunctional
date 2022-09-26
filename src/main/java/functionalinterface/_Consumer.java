bb25434ddb843db097fd7497f0bfbe24900c22d2
Jimmy Botezine <jbotezine@gmail.com>
2022-09-26 08:53:20 -0300

    feat: add Consumer

---
+++ src/main/java/functionalinterface/_Consumer.java
@@ -0,0 +1,44 @@
+package functionalinterface;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+public class _Consumer {
+
+    public static void main(String[] args) {
+        //Normal Java function
+        Customer maria = new Customer("Maria", "9999");
+        greetCustomer(maria);
+
+        // Consumer Function interface
+        greetCustomerConsumer.accept(maria);
+
+        greetCustomerConsumerv2.accept(maria, false);
+    }
+
+    static BiConsumer<Customer, Boolean> greetCustomerConsumerv2 = (customer, showPhoneNumber) ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number "
+                    + (showPhoneNumber ? customer.customerPhoneNumber : "*********"));
+
+    static Consumer<Customer> greetCustomerConsumer = customer ->
+            System.out.println("Hello " + customer.customerName +
+                    ", thanks for registering phone number "
+                    + customer.customerPhoneNumber);
+
+    static void greetCustomer(Customer customer) {
+        System.out.println("Hello " + customer.customerName +
+                ", thanks for registering phone number "
+                + customer.customerPhoneNumber);
+    }
+
+    static class Customer {
+        private final String customerName;
+        private final String customerPhoneNumber;
+
+        public Customer(String customerName, String customerPhoneNumber) {
+            this.customerName = customerName;
+            this.customerPhoneNumber = customerPhoneNumber;
+        }
+    }
+}
